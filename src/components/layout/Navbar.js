2eac0a60acd513cd018af39006727ebe72c71abb
kiharani101 <dev.kiharani@gmail.com>
2018-06-17 01:57:53 +0300

    Basic setup and router

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+        <Link className="navbar-brand" to="/" >Kilele Staff Portal</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/staff">Staff</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default  Navbar;
---
+++ src/components/pages/Staff.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <h1>Staffpage</h1>
+    </div>
+  )
+}
